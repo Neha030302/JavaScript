b75a71f3ef868e32fc1928a2da4f4b776b2b2a85
Neha030302 <94954560+Neha030302@users.noreply.github.com>
2025-08-14 09:57:54 +0000

    Intro file done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+
+{/**
+Variables in JavaScript:
+Variables store data that can be used and modified later. 
+Variables are containers for storing data values. 
+JavaScript is loosely typed, meaning variables can hold any data type (number, string, boolean, etc.).  
+*/}
